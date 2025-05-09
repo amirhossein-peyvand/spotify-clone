fa876da3f00b1c5904210fda50bee31a99af6f81
Amir Hossein Peyvand <amirhosseinpeyvand2020@gmail.com>
2025-05-09 18:58:10 +0330

    Building and styling Display, DisplayHome and Navbar components and using react-router-com for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Player from "./components/Player";
 import Sidebar from "./components/Sidebar";
+import Display from "./components/Display";
 
 const App = () => {
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar />
+        <Display />
       </div>
       <Player />
     </div>
